f4c54b0d2a75e74d911696dc36162e0bbe185efe
mimi0702 <maimounaba229@gmail.com>
2019-04-11 10:18:54 +0000

    Boucle while

--- nombre_aleatoire.py
+++ nombre_aleatoire.py
@@ -1,14 +1,16 @@
-
 import random
-
+	
 x = random.randint(0, 9)
 chiffre_secret = random.randint(0, 9)
+print(chiffre_secret)
 reponse  = None
-while reponse != chiffre_secret:
+while True:
 	reponse =int(input("entrer une valeur entre 0 et 9"))
+	if reponse == chiffre_secret:
+		print("vous avez gagné")	
 	if reponse > chiffre_secret:
-		print("le chiffre secret est plus petit que ca:")
+		print("le chiffre secret est plus petit que la reponse.")
 	elif reponse < chiffre_secret:
-		print("le chiffre secret est plus grand que ca":)
-	else:
-		print("vous avez gagne")
+		print("le chiffre secret est plus grand que la reponse.")
+	break
+
