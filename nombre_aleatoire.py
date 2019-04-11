42f83118d997bf150a9313c3022178cdfcf718ed
mimi0702 <maimounaba229@gmail.com>
2019-04-11 10:40:56 +0000

    rectification

--- nombre_aleatoire.py
+++ nombre_aleatoire.py
@@ -4,8 +4,8 @@ x = random.randint(0, 9)
 chiffre_secret = random.randint(0, 9)
 print(chiffre_secret)
 reponse  = None
+reponse =int(input("entrer une valeur entre 0 et 9"))
 while True:
-	reponse =int(input("entrer une valeur entre 0 et 9"))
 	if reponse == chiffre_secret:
 		print("vous avez gagné")	
 	if reponse > chiffre_secret:
@@ -13,4 +13,5 @@ while True:
 	elif reponse < chiffre_secret:
 		print("le chiffre secret est plus grand que la reponse.")
 	break
+
 
