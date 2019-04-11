2d94a358ed829a6e24cc04af302c5777bee79d06
mimi0702 <maimounaba229@gmail.com>
2019-04-11 08:55:14 +0000

    boucle for

---
+++ nombre_aleatoire2.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+chiffre_secret = random.randint(0, 9)
+print(chiffre_secret)
+reponse  =  None
+for i in  range(3):
+
+	reponse =int(input("entrer une valeur:"))
+	if reponse > chiffre_secret:
+		print("le chiffre secret est plus petit que ca:")
+	elif reponse < chiffre_secret:
+		print("le chiffre secret est plus grand que ca:")
+	else:
+		print("vous avez gagne")
+		sys.exit()
+
+print("Dommage vous avez perdu")
